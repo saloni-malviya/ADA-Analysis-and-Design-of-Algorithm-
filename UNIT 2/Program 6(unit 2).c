c553013affa7577bb48a1f3857fb1241d1f70b09
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2023-05-11 21:50:31 +0530

    Add files via upload

---
+++ UNIT 2/Program 6(unit 2).c
@@ -0,0 +1,72 @@
+16. Implement a divide-and-conquer algorithm for merging two sorted arrays.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge(int* arr, int low, int mid, int high) {
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+    int L[n1], R[n2];
+    for (int i = 0; i < n1; i++) {
+        L[i] = arr[low + i];
+    }
+    for (int j = 0; j < n2; j++) {
+        R[j] = arr[mid + 1 + j];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = low;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void merge_sort(int* arr, int low, int high) {
+    if (low < high) {
+        int mid = (low + high) / 2;
+        merge_sort(arr, low, mid);
+        merge_sort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+}
+
+int main() {
+    int arr1[] = {1, 3, 5, 7, 9};
+    int arr2[] = {2, 4, 6, 8, 10};
+    int n1 = sizeof(arr1) / sizeof(arr1[0]);
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+    int arr[n1 + n2];
+    for (int i = 0; i < n1; i++) {
+        arr[i] = arr1[i];
+    }
+    for (int j = 0; j < n2; j++) {
+        arr[n1 + j] = arr2[j];
+    }
+    int n = n1 + n2;
+    merge_sort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
